bb3df0a03af676460cde82e287435bc22f5d8499
Darshan_Awachat <darshanawachat@gmail.com>
2020-04-11 20:55:53 +0530

    fourth ass

---
+++ module3-solution/js/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+
+var message="in global";
+console.log("global:message =" +message)
